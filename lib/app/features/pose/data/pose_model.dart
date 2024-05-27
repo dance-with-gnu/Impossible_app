d65bb7e68fdf0bcd13770f4611ded803c4959a39
Choieeh <chyh0182@naver.com>
2024-05-27 23:42:51 +0900

    fix: pose model update

--- lib/app/features/pose/data/pose_model.dart
+++ lib/app/features/pose/data/pose_model.dart
@@ -1,4 +1,6 @@
 class PoseModel {
+  final int poseId;
+  final int poseCategoryId;
   final String poseName;
   final String poseThumbnail;
   final String poseDescription;
@@ -6,6 +8,8 @@ class PoseModel {
   final int poseViews;
 
   PoseModel({
+    required this.poseId,
+    required this.poseCategoryId,
     required this.poseName,
     required this.poseThumbnail,
     required this.poseDescription,
@@ -15,6 +19,8 @@ class PoseModel {
 
   factory PoseModel.fromJson(Map<String, dynamic> json) {
     return PoseModel(
+      poseId: json['id'],
+      poseCategoryId: json['category_id'],
       poseName: json['name'],
       poseThumbnail: json['image'],
       poseDescription: json['description'],
