f051c063db2ae04dadac2396e20f9717c906479c
Choieeh <chyh0182@naver.com>
2024-05-28 21:11:20 +0900

    feat: image cropper 사용

--- lib/app/features/pose/pose_image_picker_widget.dart
+++ lib/app/features/pose/pose_image_picker_widget.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:image_cropper/image_cropper.dart';
 
 class PoseImagePickerWidget extends StatefulWidget {
   const PoseImagePickerWidget({super.key});
@@ -15,40 +15,74 @@ class _PoseImagePickerWidgetState extends State<PoseImagePickerWidget> {
   final ImagePicker picker = ImagePicker();
 
   Future getImage(ImageSource imageSource) async {
-    //pickedFile에 ImagePicker로 가져온 이미지가 담긴다.
     final XFile? pickedFile = await picker.pickImage(source: imageSource);
     if (pickedFile != null) {
-      setState(() {
-        _image = XFile(pickedFile.path); //가져온 이미지를 _image에 저장
-      });
+      // 이미지 자르기 기능 호출
+      File? croppedFile = await cropImage(File(pickedFile.path));
+      if (croppedFile != null) {
+        setState(() {
+          _image = XFile(croppedFile.path); // 자른 이미지를 _image에 저장
+        });
+      }
     }
+  }
+
+  Future<File?> cropImage(File imageFile) async {
+    CroppedFile? croppedFile = await ImageCropper().cropImage(
+      sourcePath: imageFile.path,
+      aspectRatio: const CropAspectRatio(ratioX: 512, ratioY: 896),
+      uiSettings: [
+        AndroidUiSettings(
+          toolbarTitle: '이미지 자르기',
+          toolbarColor: Colors.deepOrange,
+          toolbarWidgetColor: Colors.white,
+          initAspectRatio: CropAspectRatioPreset.original,
+          lockAspectRatio: true,
+        ),
+        IOSUiSettings(
+          title: '이미지 자르기',
+          minimumAspectRatio: 1.0,
+          aspectRatioLockEnabled: true,
+          rotateClockwiseButtonHidden: true,
+          aspectRatioPickerButtonHidden: true,
+          aspectRatioLockDimensionSwapEnabled: true,
+        ),
+      ],
+    );
+    if (croppedFile != null) {
+      return File(croppedFile.path);
+    }
+    return null;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        const SizedBox(height: 30, width: double.infinity),
-        _buildPhotoArea(),
-        const SizedBox(height: 20),
-        _buildButton(),
-      ]),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(height: 30, width: double.infinity),
+          _buildPhotoArea(),
+          const SizedBox(height: 20),
+          _buildButton(),
+        ],
+      ),
     );
   }
 
   Widget _buildPhotoArea() {
-    return _image != null
-        ? AspectRatio(
-            aspectRatio: 512 / 896,
-            child: Image.file(File(_image!.path)), //가져온 이미지를 화면에 띄워주는 코드
-          )
-        : AspectRatio(
-            aspectRatio: 512 / 896,
-            child: Container(
-              color: Colors.grey,
-              height: 100,
-            ),
-          );
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.8,
+      child: AspectRatio(
+        aspectRatio: 512 / 896,
+        child: _image != null
+            ? Image.file(File(_image!.path))
+            : Container(
+                color: Colors.grey,
+              ),
+      ),
+    );
   }
 
   Widget _buildButton() {
@@ -57,14 +91,14 @@ class _PoseImagePickerWidgetState extends State<PoseImagePickerWidget> {
       children: [
         ElevatedButton(
           onPressed: () {
-            getImage(ImageSource.camera); //getImage 함수를 호출해서 카메라로 찍은 사진 가져오기
+            getImage(ImageSource.camera);
           },
           child: const Text("카메라"),
         ),
         const SizedBox(width: 30),
         ElevatedButton(
           onPressed: () {
-            getImage(ImageSource.gallery); //getImage 함수를 호출해서 갤러리에서 사진 가져오기
+            getImage(ImageSource.gallery);
           },
           child: const Text("갤러리"),
         ),
